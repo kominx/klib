c34d3b2c1a354cb6bcfb46dd86fc1ccaba16cfe4
kominx <kominxlpa@gmail.com>
2018-02-09 22:41:18 +0000

    first commit

---
+++ kstrings.go
@@ -0,0 +1,15 @@
+package klib
+
+// go get -u github.com/kominx/klib
+// https://github.com/esimov/colorquant
+// go build github.com/kominx/klib
+// or go build
+
+import (
+	"strings"
+)
+
+// Returns the string changed with uppercase.
+func Dummy(s string) string {
+	return strings.ToUpper(s)
+}
